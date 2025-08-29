7c5000af05f2e8b7f56eaa660b40e153bfd8eb08
Allyson Freitas <alisonfr83@gmail.com>
2025-08-29 19:42:53 -0300

    chore: add tailwindcss to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <h1 class="text-3xl font-bold text-red-400"> Hello world! </h1>
+  return <h1 class="font-sans text-3xl text-red-400"> Hello world! </h1>
 }
 
 export default App
