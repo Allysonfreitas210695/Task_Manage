7adcbdacaf17a1a812bdb5542c1aa332001831de
Allyson Freitas <alisonfr83@gmail.com>
2025-08-30 09:11:10 -0300

    feat: add taskItem component

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,3 +1,7 @@
+import CheckIcon from "../assets/icons/check.svg?react"
+import LoaderIcon from "../assets/icons/loader.svg?react"
+import DetailsIcon from "../assets/icons/details.svg?react"
+
 const TaskItem = ({ task }) => {
   const variantTaskColor = {
     done: "bg-[#00ADB5]/10 text-[#00ADB5]",
@@ -6,9 +10,29 @@ const TaskItem = ({ task }) => {
   }
   return (
     <div
-      className={`flex items-center gap-2 rounded-lg px-4 py-3 text-sm ${variantTaskColor[task.status]}`}
+      className={`flex w-full items-center justify-between gap-2 rounded-lg px-4 py-3 text-sm ${variantTaskColor[task.status]}`}
     >
-      {task.title}
+      <div className="flex items-center gap-3">
+        <label
+          htmlFor="time"
+          className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg ${variantTaskColor[task.status]}`}
+        >
+          <input
+            type="checkbox"
+            id={"time"}
+            className="absolute h-full w-full cursor-pointer opacity-0"
+            checked={task.status === "done"}
+          />
+          {task.status === "done" && <CheckIcon />}
+          {task.status === "in_progress" && (
+            <LoaderIcon className="animate-spin" />
+          )}
+        </label>
+        {task.title}
+      </div>
+      <a href="#" className="transition-colors hover:opacity-75">
+        <DetailsIcon />
+      </a>
     </div>
   )
 }
