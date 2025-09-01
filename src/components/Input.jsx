4f2bcc45a65b8b9372749cb2f7279660c541cc37
Allyson Freitas <alisonfr83@gmail.com>
2025-09-01 08:29:11 -0300

    feat: add component input, inputLabel and inputErrorMessage

---
+++ src/components/Input.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types"
+import { forwardRef } from "react"
+
+import InputErrorMessage from "./InputErrorMessage"
+import InputLabel from "./InputLabel"
+
+const Input = forwardRef(({ label, errorMessage, ...rest }, ref) => {
+  return (
+    <div className="flex flex-col space-y-1 text-left">
+      <InputLabel htmlFor={rest.id}>{label}</InputLabel>
+      <input
+        className="border-brand-border outline-brand-primary placeholder:text-brand-text-gray rounded-lg border border-solid px-4 py-3 placeholder:text-sm"
+        ref={ref}
+        {...rest}
+      />
+      {errorMessage && <InputErrorMessage>{errorMessage}</InputErrorMessage>}
+    </div>
+  )
+})
+
+Input.displayName = "Input"
+Input.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  errorMessage: PropTypes.string,
+  placeholder: PropTypes.string,
+}
+
+export default Input
