ed7eb8ff8e9b986b4dfe9bf074d1cfb71a4cfc9a
Allyson Freitas <alisonfr83@gmail.com>
2025-09-05 17:41:19 -0300

    feat: add services to tasks

---
+++ src/api/task-api.js
@@ -0,0 +1,68 @@
+import { api } from "../lib/axios"
+
+export async function createTask(newTask) {
+  try {
+    const { data } = await api.post("/tasks", newTask)
+    return data
+  } catch (error) {
+    if (error.response) {
+      throw new Error(error.response.data?.message || "Erro ao criar tarefa")
+    }
+
+    throw new Error("Erro de conexão com o servidor")
+  }
+}
+
+export async function removeTask(taskId) {
+  try {
+    const { data } = await api.delete(`/tasks/${taskId}`)
+    return data
+  } catch (error) {
+    if (error.response) {
+      throw new Error(
+        error.response.data?.message || "Erro ao remover a tarefa"
+      )
+    }
+    throw new Error("Erro de conexão com o servidor")
+  }
+}
+
+export async function updateTaskStatus(taskId, newStatus) {
+  try {
+    await api.patch(`/tasks/${taskId}`, { status: newStatus })
+  } catch (error) {
+    if (error.response) {
+      throw new Error(
+        error.response.data?.message || "Erro ao atualizar o status da tarefa"
+      )
+    }
+    throw new Error("Erro de conexão com o servidor")
+  }
+}
+
+async function deleteTaskById(taskId) {
+  try {
+    await api.delete(`/tasks/${taskId}`)
+  } catch (error) {
+    if (error.response) {
+      throw new Error(
+        error.response.data?.message || "Erro ao remover a tarefa"
+      )
+    }
+    throw new Error("Erro de conexão com o servidor")
+  }
+}
+
+export async function removeAllTasks(tasks) {
+  try {
+    await Promise.all(tasks.map((task) => deleteTaskById(task.id)))
+    return true
+  } catch (error) {
+    if (error.response) {
+      throw new Error(
+        error.response.data?.message || "Erro ao remover todas as tarefas"
+      )
+    }
+    throw new Error("Erro de conexão com o servidor")
+  }
+}
