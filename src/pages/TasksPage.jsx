e57f83aea03c5383ab2149adcd7ba298e72b1d82
Allyson Freitas <alisonfr83@gmail.com>
2025-09-06 17:36:53 -0300

    chore: removing component from task to taskpage

--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -1,8 +1,44 @@
-import SideBar from "../components/Sidebar"
-import Tasks from "../components/Tasks"
+import { CloudSunIcon, MoonIcon, SunIcon } from "../assets/icons"
+import Header from "../components/Header"
+import TaskItem from "../components/TaskItem"
+import TasksSeparator from "../components/TasksSeparator"
+import { useGetTasks } from "../hooks/queries/use-get-tasks"
 
 const TasksPage = () => {
-  return <Tasks />
+  const { data: tasks } = useGetTasks()
+
+  const morningTasks = tasks?.filter((task) => task.time === "morning")
+  const afternoonTasks = tasks?.filter((task) => task.time === "afternoon")
+  const eveningTasks = tasks?.filter((task) => task.time === "evening")
+
+  return (
+    <div className="w-full space-y-6 px-[34px] py-16">
+      <Header title="Minhas Tarefas" />
+
+      <div className="rounded-xl bg-white p-6">
+        <div className="space-y-3">
+          <TasksSeparator title={"Manhã"} icon={<SunIcon />} />
+          {morningTasks?.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
+        </div>
+
+        <div className="my-6 space-y-3">
+          <TasksSeparator title={"Tarde"} icon={<CloudSunIcon />} />
+          {afternoonTasks?.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
+        </div>
+
+        <div className="space-y-3">
+          <TasksSeparator title={"Noite"} icon={<MoonIcon />} />
+          {eveningTasks?.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default TasksPage
